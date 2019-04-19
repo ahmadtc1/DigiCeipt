46fa8f2ab1bd42c304a60315af6c8422af502689
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-19 12:43:36 -0400

    Adding receipt view and adding redirecting for invalid routes

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,16 +4,25 @@ import { SharedModule } from '../shared/shared.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from '../material/material.module';
 import { SaveFormComponent } from './save-form/save-form.component';
+import { RouterModule } from '@angular/router';
+import { ReceiptViewComponent } from './receipt-view/receipt-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaveFormComponent
+    SaveFormComponent,
+    ReceiptViewComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: AppComponent },
+      { path: 'upload', component: ReceiptViewComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
