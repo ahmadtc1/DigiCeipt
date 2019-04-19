75cb19aa2a4313bc4174ef450f0b3dc8a7cd49a9
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-19 16:57:34 -0400

    Altering html selector tag

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-menu',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
