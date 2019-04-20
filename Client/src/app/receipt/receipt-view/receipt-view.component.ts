31d22e224ed3dcfcdf2daeb3b5b9936ce274555d
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-20 11:05:48 -0400

    Adding material icons as a dependency

--- Client/src/app/receipt/receipt-view/receipt-view.component.ts
+++ Client/src/app/receipt/receipt-view/receipt-view.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Receipt } from '../receipt'
 @Component({
   templateUrl: './receipt-view.component.html',
   styleUrls: ['./receipt-view.component.css']
 })
 export class ReceiptViewComponent implements OnInit {
+  receipts: Receipt[];
+  filteredReceipts: Receipt[] = [];
 
+  
   constructor() { }
 
   ngOnInit() {
