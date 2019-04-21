c63f2ab16a1b03c77f988590a30a1f48403f8cce
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-21 14:21:48 -0400

    Add image upload in upload form

--- Client/src/app/receipt/save-form/save-form.component.ts
+++ Client/src/app/receipt/save-form/save-form.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material';
-
 @Component({
   selector: 'app-save-form',
   templateUrl: './save-form.component.html',
   styleUrls: ['./save-form.component.css']
 })
-export class SaveFormComponent {
+export class SaveFormComponent implements OnInit{
   name: string;
   content: string;
   isNameEmpty: boolean = true;
@@ -26,4 +25,7 @@ export class SaveFormComponent {
     this.content = "";
   }
 
+  ngOnInit(): void {
+
+  }
 }
