b642b31fd4d14bfabbb129b9915d0c3308471d01
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-22 23:26:06 -0400

    Adding more mock data temporarily and formatting receiptView

--- Client/src/app/receipt/receipt-view/receipt-view.component.ts
+++ Client/src/app/receipt/receipt-view/receipt-view.component.ts
@@ -15,6 +15,26 @@ export class ReceiptViewComponent implements OnInit {
       "receiptName": "receiptOne",
       "receiptContent": "10.99",
       "dateSaved": new Date()
+    },
+    {
+      "receiptName": "receiptTwo",
+      "receiptContent": "10.20",
+      "dateSaved": new Date()
+    },
+    {
+      "receiptName": "receiptThree",
+      "receiptContent": "12.7",
+      "dateSaved": new Date()
+    },
+    {
+      "receiptName": "receiptFour",
+      "receiptContent": "10.97",
+      "dateSaved": new Date()
+    },
+    {
+      "receiptName": "receiptFive",
+      "receiptContent": "5.55",
+      "dateSaved": new Date()
     }
   ];
   filteredReceipts: Receipt[] = [];
