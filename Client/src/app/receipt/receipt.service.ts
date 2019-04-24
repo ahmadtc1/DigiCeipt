960979cdc6bc1f0d3892bbf1e91284316012fe65
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-23 22:52:14 -0400

    Creating error handling function for http request

--- Client/src/app/receipt/receipt.service.ts
+++ Client/src/app/receipt/receipt.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Receipt } from './receipt';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceiptService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private handleError(err: HttpErrorResponse) {
+    //in a real world app we may send the server to some remote logging infrastructure
+    //Instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      //A client-side or network error occurred. Handle it accordingly
+      errorMessage = `An error occurred ${err.error.message}`;
+    }
+
+    else {
+      //The backend returned an unsuccessful reponse code
+      //The response body may contain clues as to what went wrong
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
